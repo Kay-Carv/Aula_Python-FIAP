421a1ff06e7ec706552c7699dda40407bc752747
Kayque <kayquecarvalho020@gmail.com>
2025-04-09 12:38:04 -0300

    Segundo exercício da avaliação

--- CP5-Prova_DataFrame&Match-Case/cp5.py
+++ CP5-Prova_DataFrame&Match-Case/cp5.py
@@ -20,3 +20,32 @@ match nota:
         print("Nota perfeita!")
     case _:
         print("Opção inválida")
+        
+        # %%
+
+"""
+4.Crie um programa que recebe o cargo de um funcionário e retorna a faixa salarial correspondente. Utilize a estrutura match case para implementar a lógica:
+Se o cargo for "CEO", a faixa salarial será "Acima de 100k."
+Se o cargo for "Gerente", a faixa salarial será "Entre 50k e 100k."
+Se o cargo for "Analista", a faixa salarial será "Entre 30k e 50k."
+Se o cargo for "Estagiário", a faixa salarial será "Menos de 30k."
+Caso o cargo não esteja na lista, exiba "Cargo não encontrado."
+"""
+
+cargo = str(input("Digite o cargo do funcionário: ")).lower()
+
+match cargo:
+    case "ceo":
+      print(f"A faixa salarial do cargo de {cargo.upper()} é acima de 100 mil reais")
+    
+    case "gerente":
+      print(f"A faixa salarial do cargo de {cargo.capitalize()} fica entre 50 a 100 mil reais")
+    
+    case "analista":  
+      print(f"A faixa salarial do cargo de {cargo.capitalize()} fica entre 30 a 50 mil reais")
+    
+    case "estagiario":
+      print(f"A faixa salarial do cargo de {cargo.capitalize()} fica abaixo dos 30mil reais")
+    
+    case _:
+      print("Cargo não encontrado")
